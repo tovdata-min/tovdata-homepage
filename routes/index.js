944ce2855e7a4ecab935eb01519072b6dd37736e
tovdata-min <tovdata.min@gmail.com>
2020-05-19 20:20:46 +0900

    Add about page

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,16 @@ router.get('/home', function(req, res) {
     res.render('home');
 });
 
+router.get('/about', function(req, res) {
+    res.render('about');
+});
+
 router.get('/about/history', function(req, res) {
     res.render('about-history');
+});
+
+router.get('/about/join', function(req, res) {
+    res.render('about-joinUs');
 });
 
 router.get('/products', function(req, res) {
