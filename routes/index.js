370d77582632dc03a2dcd90ee8836770a060b671
tovdata-min <tovdata.min@gmail.com>
2020-05-20 16:15:46 +0900

    Modify main menu and add pages

--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,31 @@ router.get('/', function(req, res) {
 });
 
 router.get('/home', function(req, res) {
-    res.render('home');
+    res.render('home', {pageName: "home"});
 });
 
 router.get('/about', function(req, res) {
-    res.render('about');
+    res.render('about', {pageName: "about"});
 });
 
 router.get('/about/history', function(req, res) {
-    res.render('about-history');
+    res.render('about-history', {pageName: "about-history"});
 });
 
 router.get('/about/join', function(req, res) {
-    res.render('about-joinUs');
+    res.render('about-joinUs', {pageName: "about-join"});
 });
 
 router.get('/products', function(req, res) {
-    res.render('products');
+    res.render('products', {pageName: "products"});
+});
+
+router.get('/team', function(req, res) {
+    res.render('team', {pageName: "team"});
+});
+
+router.get('/contact', function(req, res) {
+    res.render('contact', {pageName: "contact"});
 });
 
 module.exports = router;
